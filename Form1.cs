4920b9c7ea1f2c294f326f08cfb306008e592b06
Recep Can <61096668+recepcantkn@users.noreply.github.com>
2022-03-29 10:10:08 +0300

    Kompleks veri bağlama

---
+++ Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Kompleks_veri_baglama2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<Sehirler> listSehirler = new List<Sehirler>();
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            Sehirler sehir = new Sehirler();
+            sehir.Plaka = txtPlaka.Text;
+            sehir.SehirAdi = txtSehirAdi.Text;
+            listSehirler.Add(sehir);
+            Bagla();
+        }
+
+        private void Bagla()
+        {
+            cbSehirler.DataSource = null;
+            cbSehirler.DataSource = listSehirler;
+            cbSehirler.DisplayMember = "SehirAdi";
+            cbSehirler.ValueMember = "Plaka";
+        }
+    }
+}
---
+++ Sehirler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kompleks_veri_baglama2
+{
+    class Sehirler
+    {
+        public string Plaka { get; set; }
+        public string SehirAdi { get; set; }
+    }
+}
